cdbad330f09d5a5b8dc40a021311600fc7d9a0c6
peko <gasu.basu@gmail.com>
2016-06-29 12:12:37 +0300

    update

--- test/glfw/show-mesh.sh
+++ test/glfw/show-mesh.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 src="show-mesh"
 dep="glad.c"
-clang `pkg-config --cflags glfw3 glu` -o $src $src.c $dep `pkg-config --libs glfw3 glu` -lm
+clang \
+`pkg-config --cflags glfw3 glu` \
+-o $src $src.c $dep \
+`pkg-config --libs glfw3 glu` -lm && \
+./$src
